382a5bd38489d42ebc94d3876e58be49afbd6a57
Mark Buckle <markbuckle92@gmail.com>
2024-09-11 13:37:04 -0230

    reduce latency issues

---
+++ README.md
@@ -0,0 +1,50 @@
+# Deploying the Ai Chatbot
+
+For this custom deployment I used my AiAppDeploy chatbot [Next.js](https://nextjs.org/) project that was bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+Here is the AWS link to my app: 
+
+https://main.d15um7o1z4plf1.amplifyapp.com/
+
+Here are my custom domains:
+
+<li>https://podcastaichatbot.website/</li>
+<li>https://www.podcastaichatbot.website/</li>
+
+## Getting Started
+
+Open the frontend folder of your AiChatBot. In my case it was the /rag-app-frontend folder.
+
+Next, run the build server:
+
+```pwsh
+npm run dev
+```
+
+If all runs smoothly run the prodcution server next:
+
+```pwsh
+npm run build
+```
+then
+```pwsh
+npm start
+````
+
+Open the link provided to see the result.
+
+## Deploy on Vercel
+
+I didn't use this method as it is slightly more expensive though easier to navigate. You can use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js. Check out [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+## Deploy on AWS Amplify
+
+AWS Amplify provides a straightforward way to deploy and host your Next.js application. You can connect your GitHub repository to Amplify, and it will handle the build and deployment process.
+
+If you don’t already have a domain, you can register one through AWS Route 53. In the Route 53 console, create a hosted zone for your domain. Click on “Hosted zones” and then “Create hosted zone”. Enter your domain name and create the hosted zone. Add DNS records to route traffic to your application. 
+
+## Create a custom Domain
+
+You can do this on AWS Amplify but you may be able to do it cheaper on nampecheap.com. 
+
+Use Copilot to find the steps for adding a custom namecheap domain to AWS Amplify
---
+++ src/components/app/submitQueryForm.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "../ui/button";
+import { Textarea } from "../ui/textarea";
+import createApiClient from "@/lib/getApiClient";
+import { useRouter } from "next/navigation";
+import { Loader2 } from "lucide-react";
+import { getSessionId } from "@/lib/getUserID";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+
+// make the api and get the userid
+export default function SubmitQueryForm() {
+  const api = createApiClient();
+  const userId = getSessionId();
+  // placeholder question for the form
+  const originalPlaceHolder: string = "How do I increase neuroplasticity?";
+
+  // hook 1:  store the query text inside of our form
+  const [query, setQuery] = useState("");
+  // hook 2:  store the boolean for whether or not were in the middle of submitting a query
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  // make the page navigate away after submitting a query
+  const router = useRouter();
+
+  // submit the query
+  const submitForm = () => {
+    // we'll use the query if it's not null, otherwise we'll use the placeholder question instead
+    const queryToSubmit = query || originalPlaceHolder;
+    console.log(`Submitting query: ${queryToSubmit}`);
+    // create the api request
+    const request = { queryText: queryToSubmit, userId: userId };
+    // send the api request
+    const response = api.submitQueryEndpointSubmitQueryPost({
+      submitQueryRequest: request,
+    });
+
+    // tell the app we hit the submit button
+    setIsSubmitting(true);
+    response.then((data) => {
+      console.log(data);
+      // navigate to the viewQuery page and add queryid to the url parameter
+      router.push(`/viewQuery?query_id=${data.queryId}`);
+    });
+  };
+
+  // styling
+  const textArea = (
+    <Textarea
+      placeholder={originalPlaceHolder}
+      value={query} // current value of the text area
+      disabled={isSubmitting} // disable form while submitting
+      // set the callback function so that whenever the query is changed we update the query state
+      onChange={(e) => {
+        setQuery(e.currentTarget.value);
+      }}
+    />
+  );
+
+  // disable submit button if form is being submitted and link it to submitForm
+  const submitButton = (
+    <Button onClick={submitForm} disabled={isSubmitting} className="ml-4">
+      {/* the button will display a loading spinner only if its in the process of being submitted */}
+      {isSubmitting && <Loader2 className="mr-2 h-4 w-4 animate-spin" />} Submit
+    </Button>
+  );
+
+  return (
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle>Submit New Query</CardTitle>
+        <CardDescription>
+          Ask a question about Andrew Huberman's podcast. A podcast aimed to
+          give help you navigate your health journey.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="w-full">{textArea}</div>
+      </CardContent>
+      <CardFooter>
+        Our AI chatbot uses the Amazon Bedrock Titan Text Express V1 LLM, which
+        typically takes 10-15 seconds to generate a response. This model offers
+        a balance between cost-effectiveness and performance. While it may not
+        match the advanced capabilities of premium models like Claude in terms
+        of language understanding and generation, it provides a practical
+        solution for many applications. The trade-off is lower costs at the
+        expense of some performance metrics compared to more sophisticated
+        models available through Amazon Bedrock. {submitButton}
+      </CardFooter>
+    </Card>
+  );
+}
