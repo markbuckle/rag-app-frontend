1b3100de21233762da279088710344e672b2247e
Mark Buckle <markbuckle92@gmail.com>
2024-09-12 11:15:09 -0230

    Repo name change update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,13 +43,13 @@ function InnerLayout({
       <footer className="w-full max-w-3xl text-sm text-slate-400 mt-4">
         <div className="text-center space-y-2">
           <div className="flex items-center gap-2 justify-center sm:flex-row flex-col">
-            <Link href="https://github.com/markbuckle/AiAppDeploy/blob/main/image/src/data/source">
+            <Link href="https://github.com/markbuckle/rag-app-backend/blob/main/image/src/data/source">
               <div className="flex hover:underline hover:text-slate-700">
                 <FileText className="mr-1 h-4 w-4 my-auto" />
                 Source PDF
               </div>
             </Link>
-            <Link href="https://github.com/markbuckle/AiAppDeploy">
+            <Link href="https://github.com/markbuckle/rag-app-backend">
               <div className="flex hover:underline hover:text-slate-700">
                 <Code className="mr-1 h-4 w-4 my-auto" />
                 Project Source Code
